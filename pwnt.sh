c5d17d492c3721ad4152b498979f9140eb7add62
Jason <jason@duedil.com>
2012-05-28 14:41:19 +0100

    first commit

---
+++ pwnt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Checks a URI for the existance of a troll link.
+#
+# EXAMPLE USAGE:
+# ./pwnt.sh www.youtube.com/watch?v=oHg5SJYRHA0
+#
+# TODO:
+# Realtime Feedback
+# Bayesian inference
+#
+string=$(wget -O - -o /dev/null $1)
+found=1
+
+declare -a evil=('roll' 'black' 'friday')
+
+for needle in "${evil[@]}"
+  do
+  if [[ "$string" == *"$needle"* ]]; then
+    echo "contains '$needle'"
+    found=0
+  fi
+done
+
+if [[ found ]]; then
+  echo "safe"
+fi
